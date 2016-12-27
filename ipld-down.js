7ddb59e8fdfaa78123e5401f3eda6bda053eba9c
kumavis <aaron@kumavis.me>
2016-12-27 11:36:41 -0800

    clean - ipld-down

--- ipld-down.js
+++ ipld-down.js
@@ -18,30 +18,15 @@ util.inherits(IpldDown, AbstractLevelDown)
 IpldDown.prototype._put = function(key, value, opts, cb){
   let ipldObj = new IpfsBlock(value)
   let cid = cidForHash('eth-state-trie', key)
-  // console.log('put:', key.toString('hex').slice(0,4), '<-', value)
   this._blockService.put({ block: ipldObj, cid: cid }, cb)
 }
 
 IpldDown.prototype._get = function(key, opts, cb){
   const self = this
   let cid = cidForHash('eth-state-trie', key)
-  // console.log('get:', key.toString('hex').slice(0,4))
-  // self._blockService.get(cid, (err, result) => {
-    // console.log('get:', key.toString('hex').slice(0,4), '->', result)
-  //   cb(err, result)
-  //   // console.log('ipldDown.get', err, result)
-  // })
   async.waterfall([
     (cb) => self._blockService.get(cid, cb),
-    (ipldBlock, cb) => {
-      // console.log('ipldBlock.data:', ipldBlock.data)
-      cb(null, ipldBlock.data)
-    },
-  // ], (err, result) => {
-    // console.log('get:', key.toString('hex').slice(0,4), '->', result)
-    // cb(err, result)
-    // console.log('ipldDown.get', err, result)
-  // })
+    (ipldBlock, cb) => cb(null, ipldBlock.data),
   ], cb)
 }
 
